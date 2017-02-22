f22cdba99628e5bcaf7264c4f900bc242c4aeb5f
Misic <m18883993593@163.com>
2017-02-22 19:29:49 +0800

    init

---
+++ pages/CardNavPage.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    Image,
+    View
+} from 'react-native';
+
+import Swiper from 'react-native-swiper';
+import DetailsPage from './DetailsPage';
+
+var styles = StyleSheet.create({
+    wrapper: {
+        backgroundColor: '#faf8f9'
+    },
+    slide1: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    slide2: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    slide3: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: 200,
+        height: 300
+    },
+    buttonText: {
+        color: '#666',
+        fontSize: 36
+    }
+});
+
+export default class CardNavPage extends Component {
+    render() {
+        return (
+            <Swiper style={styles.wrapper} showsButtons={true} showsPagination={false} nextButton={<Text style={styles.buttonText}>›</Text>} prevButton={<Text style={styles.buttonText}>‹</Text>}>
+                <View style={styles.slide1}>
+                    <Image style={styles.image} source={require('./imgs/1.png')} />
+                </View>
+                <View style={styles.slide2}>
+                    <Image style={styles.image} source={require('./imgs/2.png')} />
+                </View>
+                <View style={styles.slide3}>
+                    <Image style={styles.image} source={require('./imgs/3.png')} />
+                </View>
+            </Swiper>
+        )
+    }
+}
+
